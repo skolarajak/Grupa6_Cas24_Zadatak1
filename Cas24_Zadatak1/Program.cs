1041f6c32b9c454d4758cffbf358a3138b83480c
Slobodan Todorov <skolarajak.qa@gmail.com>
2020-04-27 21:53:04 +0200

    Add project files.

---
+++ Cas24_Zadatak1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cas24_Zadatak1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int kilograms = 0;
+            Console.Write("Unesite vrednost u kilogramima > ");
+            kilograms = Convert.ToInt32(Console.ReadLine());
+
+            int grams = WeightConversion.KG2GR(kilograms);
+
+            Console.WriteLine("{0} kg iznosi {1} gr", kilograms, grams);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Cas24_Zadatak1/WeightConversion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cas24_Zadatak1
+{
+    class WeightConversion
+    {
+        public static int KG2GR(int kilograms)
+        {
+            return kilograms * 1000;
+        }
+    }
+}
